dd008916ed80d601b7ea95516948255a1ebbf057
evagil <evaeg19@gmail.com>
2022-04-21 17:44:16 -0300

    modificacion de state

--- tercer_proyecto_react/src/index.js
+++ tercer_proyecto_react/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Estudiante nombre="Evangelina" apellido="Gil" carrera="Lic. en sistemas" />
-    {/* <Tabla />*/}
+    <Tabla />
   </React.StrictMode>
 );
 
